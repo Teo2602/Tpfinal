b9a00a8fe917308f6a3bedb502fa4a025d77ae75
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2025-11-27 10:31:22 -0300

    packman te sigue y resetea la escena

---
+++ Assets/agentmanager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class agentmanager : MonoBehaviour
+{
+    [SerializeField] Transform obj;
+    [SerializeField] NavMeshAgent agente;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.destination = obj.position;
+        if(agente.path!=null && agente.remainingDistance < 0.5)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
